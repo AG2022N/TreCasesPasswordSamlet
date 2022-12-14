95a257836888c512b45936cb44435e50c0a1c65e
Allan <AGawron2021@protonmail.com>
2022-12-14 08:11:35 +0100

    Major changes

--- TreCasesPasswordSamlet/Program.cs
+++ TreCasesPasswordSamlet/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace TreCasesPasswordSamlet
 {
@@ -13,54 +11,79 @@ namespace TreCasesPasswordSamlet
         static void Main()
         {
             bool PasswordValid = false;
-            //while (PasswordValid == false)
-            //{
-            //    PasswordValid = PasswordChecks.PasswordCreate();
-            //    Console.ReadKey();
-            //    Console.Clear();
-            //}
-            while (PasswordValid == false)
+            int UserChoice = 0;
+            while (UserChoice < 3)
             {
-                PasswordValid = PasswordChecks.PasswordCheck();
-                Console.ReadKey();
+                Console.SetCursorPosition(5, 1);
+                Console.WriteLine("Tast 1 for at logge ind");
+                Console.SetCursorPosition(5, 2);
+                Console.WriteLine("Tast 2 for at skabe en password");
+                Console.SetCursorPosition(5, 3);
+                Console.WriteLine("Tast 3 for at lukke programmen");
+                Console.SetCursorPosition(5, 4);
+                UserChoice = Convert.ToInt32(Console.ReadLine());
                 Console.Clear();
-            }
 
-            int userChoice;
-            Console.SetCursorPosition(5, 1);
-            Console.WriteLine("Tast 1 for at køre fodbold programmen");
-            Console.SetCursorPosition(5, 2);
-            Console.WriteLine("Tast 2 for at køre danse programmen");
-            Console.SetCursorPosition(5, 3);
-            userChoice = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-            switch (userChoice)
-            {
-                case 1:
-                    for (int i = 0; i < 99; i++)
-                    {
-                        string B = Fodbold.FodboldProgram();
-                        Console.SetCursorPosition(5, 2);
-                        Console.WriteLine(B);
-                        Console.ReadKey();
-                    }
+                if (UserChoice == 3)
+                {
                     break;
+                }
 
-                case 2:
-                    int Max;
-                    Console.SetCursorPosition(5, 1);
-                    Console.WriteLine("Hvor mange par vil du gerne oprette?");
-                    Console.SetCursorPosition(5, 2);
-                    Max = Convert.ToInt32(Console.ReadLine());
-                    for (int i = 0; i < Max; i++)
-                    {
-                        Console.Clear();
-                        string danserPar = Danser.DanserProgram();
+                switch (UserChoice)
+                {
+                    case 1:
+                        while (PasswordValid == false)
+                        {
+                            PasswordValid = PasswordChecks.PasswordCheck();
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                        break;
+
+                    case 2:
+                        while (PasswordValid == false)
+                        {
+                            PasswordValid = PasswordChecks.PasswordCreate();
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                        break;
+                }
+                Console.SetCursorPosition(5, 1);
+                Console.WriteLine("Tast 1 for at køre fodbold programmen");
+                Console.SetCursorPosition(5, 2);
+                Console.WriteLine("Tast 2 for at køre danse programmen");
+                Console.SetCursorPosition(5, 3);
+                UserChoice = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (UserChoice)
+                {
+                    case 1:
+                        for (int i = 0; i < 99; i++)
+                        {
+                            string B = Fodbold.FodboldProgram();
+                            Console.SetCursorPosition(5, 2);
+                            Console.WriteLine(B);
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case 2:
+                        int Max;
+                        Console.SetCursorPosition(5, 1);
+                        Console.WriteLine("Hvor mange par vil du gerne oprette?");
                         Console.SetCursorPosition(5, 2);
-                        Console.Write(danserPar);
-                        Console.ReadKey();
-                    }
-                    break;
+                        Max = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < Max; i++)
+                        {
+                            Console.Clear();
+                            string danserPar = Danser.DanserProgram();
+                            Console.SetCursorPosition(5, 2);
+                            Console.Write(danserPar);
+                            Console.ReadKey();
+                        }
+                        break;
+                }
             }
         }
     }
